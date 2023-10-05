d45e9a886200df3ae39222f2bf3e247016df9ce4
HVEPYC <vinodharishankar@gmail.com>
2023-10-05 23:53:01 +0800

    Uploads before making Public

---
+++ ContentBasedFilteringRecommender.py
@@ -0,0 +1,111 @@
+#Created by HVEPYC
+
+#Required Imports
+import pandas
+
+#Importing the Dataset that contains the movies which the Movie Recommender recommends in the Collaborative Filtering Method
+print("IMPORTS HAPPENING HERE #####################################")
+MoviesAndGenreDF = pandas.read_csv(".\\ml-25m\\movies.csv", usecols=["movieId","title","genres"], dtype={"movieId":"int32","title":"str","genres":"str"})
+
+#Putting every movie next to the Genre it is listed with into a new Dataframe
+print("MOVIE NEXT TO GENRE IN DIFFERENT ROWS HAPPENING HERE #####################################")
+MoviesWithEachGenreDF = MoviesAndGenreDF.set_index(["movieId","title"]).apply(lambda x: x.str.split("|").explode()).reset_index()
+
+#Removing the movies which have (No Genres Listed) in the genre tab
+print("REMOVAL OF (NO GENRES LISTED) HAPPENING HERE #####################################")
+MoviesWithEachGenreDFtoDelete = MoviesWithEachGenreDF[ (MoviesWithEachGenreDF["genres"] == "(no genres listed)") ].index
+MoviesWithEachGenreDF.drop(MoviesWithEachGenreDFtoDelete, inplace=True)
+
+#Creation of Movie Database with Average Ratings for Each Movie from the movies in MoviesWithEachGenreDF
+print("MOVIES AND RATINGS DATAFRAME BEING CREATED HERE ####################################")
+#Importing The Reviews Dataset:
+ratingsDF = pandas.read_csv(".\\ml-25m\\ratings.csv", usecols=["userId","movieId","rating"],dtype={"userId":"int32","movieId":"int32","rating":"float32"})
+
+#Merging RatingsDF with the MoviesWithEachGenreDF:
+MoviesAndAllRatingsDF = MoviesWithEachGenreDF.merge(ratingsDF, on="movieId")
+
+#Finding Average Rating for each movie:
+MoviesAndAvgRatingsDF = MoviesAndAllRatingsDF.groupby(["title"])["rating"].mean().sort_values(ascending=False).reset_index().rename(columns={"rating":"avgrating"})
+
+#Creation of a Pivot Table containing Movies to Genres
+print("PIVOT TABLE CREATION HAPPENING HERE #####################################")
+
+#Addition of 1s in the table for Pivot Table 1 and 0 Differentiation
+MoviesWithEachGenreDF["exists"] = 1
+
+#Creating a Pivot table using MoviesWithEachGenreDF
+MoviesWithEachGenrePivotTable = MoviesWithEachGenreDF.pivot_table(index="title", columns="genres", values="exists").fillna(0)
+
+#Converting the Pivot Table back to a normal Dataframe
+#We first remove "genres" which will give:
+MoviesWithEachGenrePivotTable.columns.name = None
+
+#Convert the Titles to have an index of their own:
+MoviesWithEachGenreFinalDF = MoviesWithEachGenrePivotTable.reset_index()
+
+#Content Based Recommendation Model Processes Here (Sample of Implementation):
+print("Content Based Recommendation HAPPENING HERE #####################################\n")
+
+#Simple Prototype Menu to enter Genres
+print("Here are the Genres: ")
+print("1. Action\n2. Adventure\n3. Animation\n4. Children\n5. Comedy\n6. Crime\n7. Documentary\n8. Drama\n9. Fantasy\n10. Film-Noir\n11. Horror\n12. IMAX\n13. Musical\n14. Mystery\n15. Romance\n16. Sci-Fi\n17. Thriller\n18. War\n19. Western")
+
+InputGenreList=[]
+
+while True:
+    try:
+        a=int(input("Enter Genre Number one by one, press enter after each genre input. Enter -1 to Stop: "))
+        if a == -1:
+            break
+        elif a<-1 or a>19:
+            print("Invalid Input")
+        else:
+            InputGenreList.append(a)
+    except ValueError:
+        continue
+
+InputGenreList = list(dict.fromkeys(InputGenreList))
+#Test Output (to be ignored)
+print(InputGenreList,"\n")
+
+#Dictionary of Genre to corresponding number:
+GenreDict = {1:"Action",2:"Adventure",3:"Animation",4:"Children",5:"Comedy",6:"Crime",7:"Documentary",8:"Drama",9:"Fantasy",10:"Film-Noir",11:"Horror",12:"IMAX",13:"Musical",14:"Mystery",15:"Romance",16:"Sci-Fi",17:"Thriller",18:"War",19:"Western"}
+
+#Generating List with Corresponding Genres as per selection
+RealGenreList = []
+for i in InputGenreList:
+    z = GenreDict[i]
+    RealGenreList.append(z)
+print(RealGenreList,"\n\n")
+
+#Finding the "Score" of specific Columns based on the given RealGenreList Input
+print("FINDING SCORE AND RATINGS HAPPENING HERE ##########################################\n")
+MoviesWithEachGenreFinalDF["Score"] = MoviesWithEachGenreFinalDF[RealGenreList].sum(axis=1)
+
+#Removing Genre Columns after Finding the Score
+MoviesWithEachGenreFinalDF.drop(MoviesWithEachGenreFinalDF.columns[1:20], axis=1, inplace=True)
+
+#Merging the Average Ratings along with the MoviesWithEachGenreFinalDF Dataframe to get results
+MoviesScoreAndRatingsDF = MoviesWithEachGenreFinalDF.merge(MoviesAndAvgRatingsDF, on="title")
+
+#Sorting the Table on the basis of Highest Score and Highest Ratings
+MoviesScoreAndRatingsDF.sort_values(by = ["Score","avgrating"], inplace=True, ascending=[False, False])
+
+#Print out the Movies in a more Readable Manner:
+print("The Movies Recommended based on your Genre Choices are as follows: \n")
+Movies = MoviesScoreAndRatingsDF.head(10)["title"].values.tolist()
+for i in Movies:
+    print(i)
+
+
+#Bibliography:
+# https://grouplens.org/datasets/movielens/25m/
+# https://www.includehelp.com/python/split-cell-into-multiple-rows-in-pandas-dataframe.aspx
+# https://www.geeksforgeeks.org/drop-rows-from-the-dataframe-based-on-certain-condition-applied-on-a-column/
+# https://stackoverflow.com/questions/24039023/add-column-with-constant-value-to-pandas-dataframe
+# https://www.w3schools.com/python/python_howto_remove_duplicates.asp
+# https://www.youtube.com/watch?v=7flWNolPhsc
+# https://www.statology.org/pandas-sum-specific-columns/
+# https://www.geeksforgeeks.org/how-to-drop-one-or-multiple-columns-in-pandas-dataframe/
+# https://sparkbyexamples.com/pandas/conver-pandas-column-to-list/
+# https://sparkbyexamples.com/pandas/pandas-sort-dataframe-by-multiple-columns/
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# HVEPYC-Content-Based-Filter-System
+# HVEPYC-Content-Based-Filtering-System
+ 
  This is a Content-Based Filtering System which was initially designed as part of a Data Science project for Semester 1 2022/23. This code was initially created to supplement the recommendation system of a Movie Recommendations website.
+
+ The code in this example uses the dataset from MovieLens.org (https://grouplens.org/datasets/movielens/25m/) as an example for Content Based Filtering. This Filtering method can also be adapted to other Datasets, as long as information regarding Genres/Class/Type of data are present for the given unit of Data.
+
+ The Code can be tested by Downloading the MovieLens dataset from the above link, and placing the unzipped "ml-25m" folder next to the Python Script. The Folder should contain both movies.csv and ratings.csv files.
+
+ This code also includes a sample implementation where the user is asked to input their desired Genres, and the Program Generates the Top 10 movies which meet those criterias, arranged from highest to lowest Rating.
+
+ This code can be adapted to be implemented in various other scenarios, and will work on any kind of data, as long as the user is given choice to select between different types of the item which is being recommended, and the dataset contains uniform genre/class/type information, along with a rating for each item. The rating could be generated as an aggregate of multiple user ratings, as shown in the example given, or can be sourced from a dataset which provides predefined ratings for each item.
+ 
+ This Filtering system has the following prerequisites:
+ - Requires a Modern version of Python (Tested on Python 3.10)
+ - Requires a significant amount of RAM, which can vary by Dataset (Above example requires a minimum of 6GB available RAM space (Or RAM + Swap Memory))
+ - A Modern CPU, to achieve reasonable runtimes for the processing of Data.
+
+ This code is created completely by Harishankar Vinod (HVEPYC), and can be used under the MIT License as stated in this repository.
+
+ References:
+ Dataset Obtained from: https://grouplens.org/datasets/movielens/25m/
+ Code-Specific references provided within thre ContentBasedFilteringRecommender.py Python file.
